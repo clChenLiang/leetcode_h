98e681e874f2239ef44e65da3837312dad9011d2
clchenliang <clchenliang@hotmail.com>
2019-09-09 01:22:20 +0800

    feat: 使用 cache 完成

--- Q-3/webpack.config.js
+++ Q-3/webpack.config.js
@@ -12,6 +12,33 @@ module.exports = {
 
   optimization: {
     runtimeChunk: true,
-    splitChunks: {},
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          filename: "[name].[chunkhash:8].js",
+          chunks: "all",
+          enforce: true
+        },
+        common: {
+          test: /[\\/]common/,
+          filename: "[name].[chunkhash:8].js",
+          chunks: "all",
+          enforce: true
+        },
+        module1: {
+          test: /[\\/]module-1/,
+          filename: "[name].[chunkhash:8].js",
+          chunks: "all",
+          enforce: true
+        },
+        module2: {
+          test: /[\\/]module-2/,
+          filename: "[name].[chunkhash:8].js",
+          chunks: "all",
+          enforce: true
+        }  
+      }
+    }   
   },
 }
