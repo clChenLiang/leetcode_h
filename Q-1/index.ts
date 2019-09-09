48f454a3de23d4a9fd55bd78ebcb608c55d9c50f
陈亮16 <chenliang16@hikvision.com.cn>
2019-09-09 13:08:26 +0800

    feat: 完成基础版

--- Q-1/index.ts
+++ Q-1/index.ts
@@ -9,4 +9,25 @@ export interface ErrorMessage {
 
 export function parseError(err: Error): ErrorMessage {
   // implement
+  let message:string = <string>err.message;
+  let result:ErrorMessage = {
+    message: message,
+    stack: []
+  }
+  // 预处理：分段
+  let statckArray:string[] = err.stack.split('\n')
+  statckArray.forEach( statck => {
+    let stackInfos:string[] = statck.replace(/\s*at\s*/, '') // 去除 chrome 与 fireFox 的区别
+      .replace(/\:\d\:\d/, str => str.replace(/\:/g, ' ')) // 分离文件
+      .split(/[@\s]/) // 拆分信息
+    // 符合标准。逃逸：“http://192.168.31.8:8000/a.js:22:3”
+    if (stackInfos.length === 4) {
+      result.stack.push({
+        line: parseInt(stackInfos[2]),
+        column: parseInt(stackInfos[3]),
+        filename: stackInfos[1]
+      })
+    }
+  })
+  return result
 }
