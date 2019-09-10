2fd2119541ef8f1f2c3a3d45fe31ed65d9fef294
clchenliang <clchenliang@hotmail.com>
2019-09-11 00:33:29 +0800

    feat: 完成 autocomplete 主体逻辑

---
+++ Q-4/autoComplete/src/autocomplete-suggestion.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="auto-complete-drop" :style="{width: parentWidth}">
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'AutoCompleteSuggestion',
+    props: {
+        
+    }
+}
+</script>
---
+++ Q-4/autoComplete/src/autocomplete.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="auto-complete">
+    <!-- 因为单独实现一个 autocomplete，固此处使用 input 代替 el-input -->
+    <div>
+        <input
+            v-model="input"
+            :placeholder="tips.input"
+            @icon-click="clearInput"/>
+            <!-- 假设图标宽为 24 px -->
+            <i class="icon-clear" style="margin-left: -24px;"></i>
+    <div>
+    <!-- 建议框 -->
+    <suggestion
+        ref="suggestion"
+        :keys="input"
+        @getData="getSuggestionData"
+        @comfirmOne="comfirm"
+        :suggestions="suggestions">
+    </suggestion>
+</div>
+</template>
+<script>
+import Suggestion from './autocomplete-suggestion'
+export default {
+    name: 'AutoComplete',
+    components: {
+        Suggestion
+    },
+    props: {
+        // AutoComplete 组件暴露的接口
+        getData: {
+            type: Function,
+            // 默认一条，自身的数组
+            default: (val) => [val]
+        }
+    },
+    data() {
+        return {
+            // 最关键的字段，输入框输入
+            input: '',
+            // 提示用语集合，便于统一处理
+            tips: {
+                input: '请输入'
+            },
+            // 下拉框的内容
+            suggestions: []
+        }
+    },
+    methods: {
+        // 消除输入
+        clearInput() {
+            this.input = ''
+        },
+        // 根据参数给出建议的
+        async getSuggestionData(val) {
+            // 可以多种方式获取。如 LocalStorage, 服务器远程调用
+            this.suggestions = await getData(val)
+        },
+        comfirm(val) {
+            this.input = val;
+        }
+    },
+    watch: {
+        'input' (val) {
+            if (!val.length) {
+                // 隐藏 suggestion
+            }
+        }
+    }
+}
+</script>
